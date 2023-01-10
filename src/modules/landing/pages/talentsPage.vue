ee210f2da15f3f74a15b1a456f76ad5449af2f18
Dynastiy <emmanuel00michael@gmail.com>
2023-01-10 08:46:59 +0100

    completed talents page

--- src/modules/landing/pages/talentsPage.vue
+++ src/modules/landing/pages/talentsPage.vue
@@ -2,6 +2,7 @@
   <div>
     <hero-section />
     <FeaturedJobs />
+    <how-it-works />
     <news-letter />
   </div>
 </template>
@@ -10,8 +11,9 @@
 import HeroSection from "../components/talents/heroSection.vue";
 import FeaturedJobs from "../components/featuredJobs.vue";
 import NewsLetter from "@/components/forms/newsLetter.vue";
+import HowItWorks from "@/components/static/howItWorks.vue";
 export default {
-  components: { HeroSection, FeaturedJobs, NewsLetter },
+  components: { HeroSection, FeaturedJobs, NewsLetter, HowItWorks },
 };
 </script>
 , NewsLetter
