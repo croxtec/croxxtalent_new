07201ca7ff46fa93705564793d4d86a86174e92f
Dynastiy <emmanuel00michael@gmail.com>
2023-01-10 10:07:34 +0100

    completed why croxxtalent section

---
+++ src/modules/landing/components/home/whyCroxx.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="why--croxx">
+    <div class="container">
+      <h1 class="header--text text-center mb-5">
+        <span>Why </span>
+        <span style="color: var(--primary-color)">CroxxTalent</span>
+      </h1>
+      <div class="row align-items-center">
+        <div class="col-md-4">
+          <div>
+            <h3 class="header--text">Layered Smart Assessments</h3>
+            <p class="small mt-3">
+              By coupling smart assessment protocols with our tiered
+              Work-Experience-Model, we are able to drive deep data profiling on
+              every individual in our ecosystem, to make intelligent matching
+              decisions for companies across the world.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <img src="@/assets/img/why-croxx.png" width="100%" alt="" />
+        </div>
+
+        <div class="col-md-4">
+          <h3 class="header--text text-right">Manage your competence</h3>
+          <p class="small mt-3 text-right">
+            High-growth companies like yours need to move at super speed.
+            CroxxTalent supports your hiring needs every step of the way, so you
+            can focus on what really matters.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/modules/landing/pages/indexPage.vue
+++ src/modules/landing/pages/indexPage.vue
@@ -4,6 +4,8 @@
     <featured-jobs />
     <TrustedBy />
     <MeetTalents />
+    <WhyCroxx />
+    <news-letter />
   </div>
 </template>
 
@@ -12,7 +14,16 @@ import heroSection from "../components/home/heroSection.vue";
 import FeaturedJobs from "../components/featuredJobs.vue";
 import TrustedBy from "../components/home/trustedBy.vue";
 import MeetTalents from "../components/home/meetTalents.vue";
+import WhyCroxx from "../components/home/whyCroxx.vue";
+import NewsLetter from "@/components/forms/newsLetter.vue";
 export default {
-  components: { heroSection, FeaturedJobs, TrustedBy, MeetTalents },
+  components: {
+    heroSection,
+    FeaturedJobs,
+    TrustedBy,
+    MeetTalents,
+    WhyCroxx,
+    NewsLetter,
+  },
 };
 </script>
