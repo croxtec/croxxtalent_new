00b170994c233e166f8af49b7eecd3f676503bb3
Dynastiy <emmanuel00michael@gmail.com>
2023-01-13 11:00:41 +0100

    completed about Us page

---
+++ src/modules/landing/components/about-us/ourValues.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container my-5">
+    <h3>Our Values</h3>
+    <div class="our-values-cards mt-4">
+      <div class="our-values-card">
+        <div class="values--icon">
+          <img src="@/assets/img/innovation.svg" alt="" />
+        </div>
+        <div>
+          <h5>Innovation</h5>
+          <p class="body--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            laudantium officia maimenda quia laborum inventore expedita, iusto
+            eveniet amet ducimus similique?
+          </p>
+        </div>
+      </div>
+
+      <div class="our-values-card">
+        <div class="values--icon">
+          <img src="@/assets/img/teamwork.svg" alt="" />
+        </div>
+        <div>
+          <h5>Team Work</h5>
+          <p class="body--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            laudantium officia maimenda quia laborum inventore expedita, iusto
+            eveniet amet ducimus similique?
+          </p>
+        </div>
+      </div>
+
+      <div class="our-values-card">
+        <div class="values--icon">
+          <img src="@/assets/img/excellence.svg" alt="" />
+        </div>
+        <div>
+          <h5>Excellence</h5>
+          <p class="body--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            laudantium officia maimenda quia laborum inventore expedita, iusto
+            eveniet amet ducimus similique?
+          </p>
+        </div>
+      </div>
+
+      <div class="our-values-card">
+        <div class="values--icon">
+          <img src="@/assets/img/reponsibility.svg" alt="" />
+        </div>
+        <div>
+          <h5>Responsibility</h5>
+          <p class="body--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            laudantium officia maimenda quia laborum inventore expedita, iusto
+            eveniet amet ducimus similique?
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/modules/landing/pages/aboutUs.vue
+++ src/modules/landing/pages/aboutUs.vue
@@ -4,6 +4,8 @@
     <OurMission />
     <OurTeam />
     <OurCoreServices />
+    <OurValues />
+    <news-letter />
   </div>
 </template>
 
@@ -12,8 +14,17 @@ import AboutHeader from "../components/about-us/aboutHeader.vue";
 import OurMission from "../components/about-us/ourMission.vue";
 import OurTeam from "../components/about-us/ourTeam.vue";
 import OurCoreServices from "../components/about-us/ourCoreServices.vue";
+import NewsLetter from "@/components/forms/newsLetter.vue";
+import OurValues from "../components/about-us/ourValues.vue";
 
 export default {
-  components: { AboutHeader, OurMission, OurTeam, OurCoreServices },
+  components: {
+    AboutHeader,
+    OurMission,
+    OurTeam,
+    OurCoreServices,
+    NewsLetter,
+    OurValues,
+  },
 };
 </script>
