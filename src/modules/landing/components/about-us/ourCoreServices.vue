0fbb5b76f3012644bdc5d2fdfce064046f57c322
Dynastiy <emmanuel00michael@gmail.com>
2023-01-13 10:43:46 +0100

    completed get hired

---
+++ src/modules/landing/components/about-us/ourCoreServices.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <h3 class="my-5">Our Core Services</h3>
+    <div class="core-services">
+      <div class="core-services-card">
+        <div>
+          <h5 class="mb-3">Provide talents to your needs</h5>
+          <p class="body--text">
+            CroxxTalent boasts of the best skilled workers. Sign up as an
+            employers and go through our robust talent database.
+          </p>
+        </div>
+        <div class="text-center">
+          <CxButton>
+            <slot>Find talents</slot>
+          </CxButton>
+        </div>
+      </div>
+      <div>
+        <img src="@/assets/img/find-you-talents.png" alt="" />
+      </div>
+      <div>
+        <img src="@/assets/img/get-you-hired.png" alt="" />
+      </div>
+      <div class="core-services-card">
+        <div>
+          <h5 class="mb-3">Get You Hired</h5>
+          <p class="body--text">
+            Looking to get hired by top engineering companies? Look no further.
+            Create your CV and start applying for jobs.
+          </p>
+        </div>
+        <div class="text-center">
+          <CxButton>
+            <slot>Get Hired</slot>
+          </CxButton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CxButton from "@/components/buttons/cxButton.vue";
+
+export default { components: { CxButton } };
+</script>
--- src/modules/landing/pages/aboutUs.vue
+++ src/modules/landing/pages/aboutUs.vue
@@ -3,6 +3,7 @@
     <AboutHeader />
     <OurMission />
     <OurTeam />
+    <OurCoreServices />
   </div>
 </template>
 
@@ -10,8 +11,9 @@
 import AboutHeader from "../components/about-us/aboutHeader.vue";
 import OurMission from "../components/about-us/ourMission.vue";
 import OurTeam from "../components/about-us/ourTeam.vue";
+import OurCoreServices from "../components/about-us/ourCoreServices.vue";
 
 export default {
-  components: { AboutHeader, OurMission, OurTeam },
+  components: { AboutHeader, OurMission, OurTeam, OurCoreServices },
 };
 </script>
