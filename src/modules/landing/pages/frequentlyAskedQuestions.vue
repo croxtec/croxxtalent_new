047c298472f7329de7703393c6df3b82575958cb
Dynastiy <emmanuel00michael@gmail.com>
2023-01-27 11:05:36 +0100

    completed faq page

--- src/modules/landing/pages/frequentlyAskedQuestions.vue
+++ src/modules/landing/pages/frequentlyAskedQuestions.vue
@@ -1,9 +1,111 @@
 <template>
   <div>
     <FaqHeader />
-    <div class="container">
-      <news-letter />
-    </div>
+
+    <section class="container">
+      <div class="faq-cards">
+        <div class="faq-card">
+          <img src="@/assets/icons/cv-d.png" alt="" />
+          <div>
+            <h5>C.V Downloads</h5>
+            <p class="body--text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et
+            </p>
+          </div>
+        </div>
+        <div class="faq-card">
+          <img src="@/assets/icons/competency.png" alt="" />
+          <div>
+            <h5>Competency Test</h5>
+            <p class="body--text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et
+            </p>
+          </div>
+        </div>
+        <div class="faq-card">
+          <img src="@/assets/icons/application.png" alt="" />
+          <div>
+            <h5>Application Management</h5>
+            <p class="body--text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et
+            </p>
+          </div>
+        </div>
+        <div class="faq-card">
+          <img src="@/assets/icons/recruitment.png" alt="" />
+          <div>
+            <h5>Recruiting Process</h5>
+            <p class="body--text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et
+            </p>
+          </div>
+        </div>
+        <div class="faq-card">
+          <img src="@/assets/icons/cv-d.png" alt="" />
+          <div>
+            <h5>Skill Assessment</h5>
+            <p class="body--text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et
+            </p>
+          </div>
+        </div>
+        <div class="faq-card">
+          <img src="@/assets/icons/cv-d.png" alt="" />
+          <div>
+            <h5>Staffing</h5>
+            <p class="body--text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="faq-list mb-5 container">
+      <h5 class="mb-4">Frequently asked questions</h5>
+      <el-collapse accordion>
+        <el-collapse-item name="1">
+          <template slot="title"> <h6>Why croxxtalent?</h6> </template>
+          <div>
+            <p>
+              Go to Estimate Fees on Cowrie 's homepage on the website to
+              estimate transfer costs and see current exchange rate. You can
+              estimate fees for sending online or via the website.
+            </p>
+          </div>
+        </el-collapse-item>
+
+        <el-collapse-item name="2">
+          <template slot="title"> <h6>Why croxxtalent?</h6> </template>
+          <div>
+            <p>
+              Go to Estimate Fees on Cowrie 's homepage on the website to
+              estimate transfer costs and see current exchange rate. You can
+              estimate fees for sending online or via the website.
+            </p>
+          </div>
+        </el-collapse-item>
+
+        <el-collapse-item name="3">
+          <template slot="title"> <h6>Why croxxtalent?</h6> </template>
+          <div>
+            <p>
+              Go to Estimate Fees on Cowrie 's homepage on the website to
+              estimate transfer costs and see current exchange rate. You can
+              estimate fees for sending online or via the website.
+            </p>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+    </section>
+
+    <news-letter />
   </div>
 </template>
 
