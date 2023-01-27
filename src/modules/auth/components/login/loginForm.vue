f9aebd3ab23eb63a7378044b12d8cd7d219733d1
Dynastiy <emmanuel00michael@gmail.com>
2023-01-27 08:45:52 +0100

    completed login

--- src/modules/auth/components/login/loginForm.vue
+++ src/modules/auth/components/login/loginForm.vue
@@ -7,7 +7,7 @@
     <div class="auth-form">
       <h4 class="text-center">Welcome Back</h4>
       <button
-        class="bordered-btn d-flex align-items-center w-100 my-5"
+        class="blue-border justify-content-center d-flex align-items-center w-100 my-4"
         style="gap: 10px"
       >
         <span> <img src="@/assets/img/google.png" width="20" alt="" /> </span>
@@ -26,6 +26,26 @@
         <div class="mb-3">
           <label for="">Password</label>
           <input type="password" placeholder="Enter password" />
+        </div>
+
+        <div class="mb-3 d-flex align-items-center" style="gap: 10px">
+          <input type="checkbox" />
+          <label for="" id="remember-me">Remember Me</label>
+        </div>
+
+        <div class="mb-3">
+          <button class="primary--button w-100">Login</button>
+        </div>
+
+        <div class="mb-2">
+          <router-link class="auth-text" to="/forgot-password"
+            >Forgotten your password?</router-link
+          >
+        </div>
+
+        <div class="d-flex align-items-center" style="gap: 4px">
+          <span class="auth-text">Don’t have an account?</span>
+          <router-link class="auth-link" to="/register">Sign Up</router-link>
         </div>
       </form>
     </div>
