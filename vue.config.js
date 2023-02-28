636c857b04dde8ba239726e124709ccb5c88cf49
Dynastiy <emmanuel00michael@gmail.com>
2023-02-28 16:03:47 +0100

    fixed public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/app/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/app/" : "/",
+  publicPath: "/",
   transpileDependencies: true,
   configureWebpack: {
     // plugins: [
