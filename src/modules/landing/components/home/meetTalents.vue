7ca0799e101d90289ae999547f8fe3636ac7ffee
vearse <versatilekaywize94@gmail.com>
2023-03-01 13:09:00 +0100

    Landing page content

--- src/modules/landing/components/home/meetTalents.vue
+++ src/modules/landing/components/home/meetTalents.vue
@@ -4,20 +4,24 @@
       <div class="meet--talent_content">
         <div class="">
           <div class="colored-card">
-            <h3 class="header--text">Meet the talents you are looking for</h3>
+            <h3 class="header--text">Find jobs, get hired and get paid - on one platform</h3>
             <img src="@/assets/img/meet_talents.png" width="100%" alt="" />
           </div>
           <div class="mt-5 text-center">
-            <cx-button> <slot>Get Started</slot> </cx-button>
+            <cx-button> 
+              <router-link to="/talents" style="color: inherit;"><slot>Find Jobs</slot></router-link>
+            </cx-button>
           </div>
         </div>
         <div class="">
           <div class="colored-card">
-            <h3 class="header--text">Find jobs, get hired and paid easily.</h3>
+            <h3 class="header--text">Source, vet and hire the best in the business</h3>
             <img src="@/assets/img/find_jobs.png" width="100%" alt="" />
           </div>
           <div class="mt-5 text-center">
-            <cx-button> <slot>Get Started</slot> </cx-button>
+            <cx-button> 
+              <router-link to="/employers" style="color: inherit;"><slot>Find Talents</slot> </router-link> 
+            </cx-button>
           </div>
         </div>
       </div>
@@ -26,7 +30,7 @@
 </template>
 
 <script>
-import cxButton from "@/components/buttons/cxButton.vue";
+import cxButton from "@/components/buttons/cxButton.vue"; 
 export default {
   components: { cxButton },
 };
