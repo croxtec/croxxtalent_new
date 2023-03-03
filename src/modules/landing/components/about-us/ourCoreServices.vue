0370688dbfcd27f83c50d191b2e46cf1ab885a61
Mfonido <favourmark05@gmail.com>
2023-03-03 17:41:19 +0100

    implemented the about us page

--- src/modules/landing/components/about-us/ourCoreServices.vue
+++ src/modules/landing/components/about-us/ourCoreServices.vue
@@ -1,39 +1,51 @@
 <template>
   <div class="container">
     <h3 class="my-5">Our Core Services</h3>
-    <div class="core-services">
-      <div class="core-services-card">
-        <div>
-          <h5 class="mb-3">Provide talents to your needs</h5>
-          <p class="body--text">
-            CroxxTalent boasts of the best skilled workers. Sign up as an
-            employers and go through our robust talent database.
-          </p>
-        </div>
-        <div class="text-center">
-          <CxButton>
-            <slot>Find talents</slot>
-          </CxButton>
+    <div class="core-services justify-content-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <div class="core-services-card">
+              <div>
+                <h5 class="mb-3">Provide talents to your needs</h5>
+                <p class="body--text">
+                  CroxxTalent boasts of the best skilled workers. Sign up as an
+                  employers and go through our robust talent database.
+                </p>
+              </div>
+              <div class="text-center">
+                <CxButton>
+                  <slot>Find talents</slot>
+                </CxButton>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm">
+              <img src="@/assets/img/find-you-talents.png" alt="" class="mt-3"/>
+          </div>
         </div>
       </div>
-      <div>
-        <img src="@/assets/img/find-you-talents.png" alt="" />
-      </div>
-      <div>
-        <img src="@/assets/img/get-you-hired.png" alt="" />
-      </div>
-      <div class="core-services-card">
-        <div>
-          <h5 class="mb-3">Get You Hired</h5>
-          <p class="body--text">
-            Looking to get hired by top engineering companies? Look no further.
-            Create your CV and start applying for jobs.
-          </p>
-        </div>
-        <div class="text-center">
-          <CxButton>
-            <slot>Get Hired</slot>
-          </CxButton>
+      <div class="container mt-3">
+        <div class="row">
+          <div class="col-sm">
+              <img src="@/assets/img/get-you-hired.png" alt="" />
+          </div>
+          <div class="col-sm">
+            <div class="core-services-card mt-3">
+              <div>
+                <h5 class="mb-3">Get You Hired</h5>
+                <p class="body--text">
+                  Looking to get hired by top engineering companies? Look no
+                  further. Create your CV and start applying for jobs.
+                </p>
+              </div>
+              <div class="text-center">
+                <CxButton>
+                  <slot>Get Hired</slot>
+                </CxButton>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/modules/landing/components/about-us/ourValues.vue
+++ src/modules/landing/components/about-us/ourValues.vue
@@ -2,61 +2,88 @@
   <div class="container my-5">
     <h3>Our Values</h3>
     <div class="our-values-cards mt-4">
-      <div class="our-values-card">
-        <div class="values--icon">
-          <img src="@/assets/img/innovation.svg" alt="" />
-        </div>
-        <div>
-          <h5>Innovation</h5>
-          <p class="body--text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-            laudantium officia maimenda quia laborum inventore expedita, iusto
-            eveniet amet ducimus similique?
-          </p>
-        </div>
-      </div>
-
-      <div class="our-values-card">
-        <div class="values--icon">
-          <img src="@/assets/img/teamwork.svg" alt="" />
-        </div>
-        <div>
-          <h5>Team Work</h5>
-          <p class="body--text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-            laudantium officia maimenda quia laborum inventore expedita, iusto
-            eveniet amet ducimus similique?
-          </p>
-        </div>
-      </div>
-
-      <div class="our-values-card">
-        <div class="values--icon">
-          <img src="@/assets/img/excellence.svg" alt="" />
-        </div>
-        <div>
-          <h5>Excellence</h5>
-          <p class="body--text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-            laudantium officia maimenda quia laborum inventore expedita, iusto
-            eveniet amet ducimus similique?
-          </p>
+      <div class="container mb-4">
+        <div class="row">
+          <div class="col-sm">
+            <div class="our-values-card">
+              <div class="values--icon">
+                <img src="@/assets/img/innovation.svg" alt="" class="img-fluid"/>
+              </div>
+              <div>
+                <h5>Innovation</h5>
+                <p class="body--text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+                  laudantium officia maimenda quia laborum inventore expedita,
+                  iusto eveniet amet ducimus similique?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm">
+            <div class="our-values-card">
+              <div class="values--icon">
+                <img src="@/assets/img/teamwork.svg" alt="" class="img-fluid"/>
+              </div>
+              <div>
+                <h5>Team Work</h5>
+                <p class="body--text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+                  laudantium officia maimenda quia laborum inventore expedita,
+                  iusto eveniet amet ducimus similique?
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div class="our-values-card">
-        <div class="values--icon">
-          <img src="@/assets/img/reponsibility.svg" alt="" />
-        </div>
-        <div>
-          <h5>Responsibility</h5>
-          <p class="body--text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-            laudantium officia maimenda quia laborum inventore expedita, iusto
-            eveniet amet ducimus similique?
-          </p>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <div class="our-values-card">
+              <div class="values--icon">
+                <img src="@/assets/img/excellence.svg" alt="" class="img-fluid"/>
+              </div>
+              <div>
+                <h5>Excellence</h5>
+                <p class="body--text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+                  laudantium officia maimenda quia laborum inventore expedita,
+                  iusto eveniet amet ducimus similique?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm">
+            <div class="our-values-card">
+              <div class="values--icon">
+                <img src="@/assets/img/reponsibility.svg" alt="" class="img-fluid"/>
+              </div>
+              <div>
+                <h5>Responsibility</h5>
+                <p class="body--text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+                  laudantium officia maimenda quia laborum inventore expedita,
+                  iusto eveniet amet ducimus similique?
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.body--text {
+  font-size: 1vw;
+}
+@media only screen and (max-width:572px) {
+  .body--text {
+    font-size: 2.5vw;
+  }
+  .col-sm {
+    margin-top: 20px;
+  }
+}
+</style>
