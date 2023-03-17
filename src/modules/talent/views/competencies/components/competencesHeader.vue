a7c591843e1a3d952e3b4898ff07971a6ca789a7
Dynastiy <emmanuel00michael@gmail.com>
2023-03-17 11:50:37 +0100

    added competence modal

--- src/modules/talent/views/competencies/components/competencesHeader.vue
+++ src/modules/talent/views/competencies/components/competencesHeader.vue
@@ -1,48 +1,60 @@
 <template>
-  <div class="d-flex align-items-center justify-content-between">
-    <div class="competences-header-container">
-      <span
-        role="button"
-        class="report-header-item"
-        :class="{ 'active-item': activeEl === '1' }"
-        @click="switchSection('1')"
-        >Skills</span
+  <div>
+    <div class="d-flex align-items-center justify-content-between">
+      <div class="competences-header-container">
+        <span
+          role="button"
+          class="report-header-item"
+          :class="{ 'active-item': activeEl === '1' }"
+          @click="switchSection('1')"
+          >Skills</span
+        >
+        <span
+          class="report-header-item"
+          :class="{ 'active-item': activeEl === '2' }"
+          role="button"
+          @click="switchSection('2')"
+          >Experience</span
+        >
+        <span
+          class="report-header-item"
+          :class="{ 'active-item': activeEl === '3' }"
+          role="button"
+          @click="switchSection('3')"
+          >Manager</span
+        >
+      </div>
+      <button
+        @click="competencesModal = !competencesModal"
+        v-if="activeEl === '1'"
+        class="primary--button d-flex align-items-center"
+        style="
+          gap: 10px;
+          background-color: var(--dark-color);
+          border-color: var(--dark-color);
+        "
       >
-      <span
-        class="report-header-item"
-        :class="{ 'active-item': activeEl === '2' }"
-        role="button"
-        @click="switchSection('2')"
-        >Experience</span
-      >
-      <span
-        class="report-header-item"
-        :class="{ 'active-item': activeEl === '3' }"
-        role="button"
-        @click="switchSection('3')"
-        >Manager</span
-      >
+        <i-icon icon="ic:baseline-plus" />
+        <span>Add Competence</span>
+      </button>
     </div>
-    <button
-      v-if="activeEl === '1'"
-      class="primary--button d-flex align-items-center"
-      style="
-        gap: 10px;
-        background-color: var(--dark-color);
-        border-color: var(--dark-color);
-      "
-    >
-      <i-icon icon="ic:baseline-plus" />
-      <span>Add Competence</span>
-    </button>
+
+    <!-- Add Competence Modal -->
+    <add-competence
+      v-if="competencesModal"
+      @close="competencesModal = !competencesModal"
+    />
   </div>
 </template>
 
 <script>
+import addCompetence from "./modal/addCompetence.vue";
 export default {
+  components: { addCompetence },
   data: () => {
     return {
       activeEl: "1",
+      competencesModal: false,
     };
   },
   methods: {
