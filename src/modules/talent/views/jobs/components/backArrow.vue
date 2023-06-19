593e568a0c141787a8d3ff00da18002b3482a090
kufre-abasi <kufreabasibassey3@gmail.com>
2023-06-19 14:47:48 +0100

    single job page

---
+++ src/modules/talent/views/jobs/components/backArrow.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <svg
+      width="36"
+      height="36"
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.355 8.89453L5.25 17.9995L14.355 27.1045"
+        stroke="#282929"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M30.7489 18H5.50391"
+        stroke="#282929"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
---
+++ src/modules/talent/views/jobs/components/jobSinglePage.vue
@@ -0,0 +1,97 @@
+<script>
+import BackIcon from "./backArrow.vue";
+
+export default {
+  components: { BackIcon },
+  data: () => {
+    return {};
+  },
+};
+</script>
+<template>
+  <div>
+    <header class="d-flex">
+      <BackIcon />
+      <h6 class="py-2 pl-2">Job Description</h6>
+    </header>
+    <div class="job-details-container">
+      <div class="d-flex justify-content-between">
+        <div class="d-flex">
+          <img src="@/assets/img/round-logo.png" />
+          <div class="pl-4">
+            <h6 class="job-title">Petroleum Engineer</h6>
+            <p class="job-sub-title">Dest oil . Paris, France</p>
+            <div class="d-flex lable-section">
+              <div class="green-label">Full-Time</div>
+              <div class="line"></div>
+              <div class="yellow-label">Mudlogging</div>
+              <div class="purple-label">Drilling</div>
+            </div>
+          </div>
+        </div>
+        <div>but</div>
+      </div>
+      <div>k</div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.heading {
+  font-size: 32px;
+  line-height: 38.4px;
+  font-weight: 700;
+}
+.job-details-container {
+  border: 1px solid #c2dbff;
+  border-radius: 40px;
+  padding: 30px 20px;
+}
+.job-title {
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 600;
+  color: #282929;
+}
+.job-sub-title {
+  font-size: 16px;
+  line-height: 25.6px;
+  font-weight: 400;
+  color: #646868;
+}
+.lable-section {
+  gap: 10px;
+}
+.green-label {
+  color: #56cdad;
+  background-color: #56cdad10;
+  padding: 9px 18px;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 22.4px;
+  border-radius: 50px;
+}
+.yellow-label {
+  color: #ffb836;
+  background-color: #ffffff;
+  padding: 9px 18px;
+  border: 1px solid #ffb836;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 22.4px;
+  border-radius: 50px;
+}
+.purple-label {
+  color: #7b61ff;
+  background-color: #ffffff;
+  padding: 9px 18px;
+  border: 1px solid #7b61ff;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 22.4px;
+  border-radius: 50px;
+}
+.line {
+  height: auto;
+  border-left: 1px solid #cbd7e7;
+}
+</style>
