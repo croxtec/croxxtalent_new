ac67046575caa8ee427e2d59929d4d757264c6c1
Dynastiy <emmanuel00michael@gmail.com>
2023-06-30 09:39:04 +0100

    removed blur effect

--- src/components/forms/contactUsForm.vue
+++ src/components/forms/contactUsForm.vue
@@ -144,9 +144,9 @@
     background: rgba(255, 255, 255, 0.52);
 border-radius: 16px;
 box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-backdrop-filter: blur(9.8px);
+/* backdrop-filter: blur(9.8px);
 -webkit-backdrop-filter: blur(9.8px);
-border: 1px solid rgba(255, 255, 255, 0.3);
+border: 1px solid rgba(255, 255, 255, 0.3); */
 }
 
 .contact--us_form form label {
