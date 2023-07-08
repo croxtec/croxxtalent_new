2bd13fba0fb50ffe8f2456dc337491ef2011f01b
vearse <versatilekaywize94@gmail.com>
2023-07-08 11:46:52 +0100

    Dev Server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require('path');
 module.exports = defineConfig({
   // publicPath: process.env.NODE_ENV === "production" ? "/app/" : "/",
   publicPath: "/",
